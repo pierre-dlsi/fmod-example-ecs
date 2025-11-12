490235d108c6145c3e2eea587d5ed0e155c2d728
pierre-dlsi <pierre@dlsi.ua.es>
2025-11-12 13:23:18 +0100

    build and run

--- buildandrun.sh
+++ buildandrun.sh
@@ -4,6 +4,5 @@ g++ -std=c++17 -o bin/game main.cpp Logger.cpp RenderSystem.cpp SoundSystem.cpp 
 -I vendor/spdlog/include/ -I vendor/entt/src -I vendor/FMOD/inc \
 -L vendor/FMOD/lib -L vendor/raylib/ -lfmod -lfmodstudio -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 cp -r assets bin/
-#export LD_LIBRARY_PATH=/home/anto/Escritorio/SoundSeum/vendor/FMOD/lib:$LD_LIBRARY_PATH 
-#export LD_LIBRARY_PATH="/media/pierre/Storage/git/fmod-example-ecs/vendor/FMOD/lib/:$LD_LIBRARY_PATH" 
+#export LD_LIBRARY_PATH=$(pwd)"/vendor/FMOD/lib/:$LD_LIBRARY_PATH" 
 #./bin/game
