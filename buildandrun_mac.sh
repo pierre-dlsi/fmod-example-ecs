8822f150510c25223c285bcfb336458797edd95c
Pedro J. Ponce de León <pierre@dlsi.ua.es>
2025-11-12 14:51:42 +0100

    build and run for mac

---
+++ buildandrun_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+g++ -std=c++17 -Wno-error=narrowing -o bin/game main.cpp Logger.cpp RenderSystem.cpp SoundSystem.cpp -I . -I vendor/raylib/src \
+-I vendor/spdlog/include/ -I vendor/entt/src -I vendor/FMOD/inc \
+-L vendor/FMOD/lib -L vendor/raylib/ -lfmod -lfmodstudio -lraylib -lGL -lm -lpthread -ldl -lX11 # -lrt
+cp -r assets bin/
+#export LD_LIBRARY_PATH=/home/anto/Escritorio/SoundSeum/vendor/FMOD/lib:$LD_LIBRARY_PATH 
+#export LD_LIBRARY_PATH="/media/pierre/Storage/git/fmod-example-ecs/vendor/FMOD/lib/:$LD_LIBRARY_PATH" 
+#./bin/game
